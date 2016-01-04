96844a790facea1820af31310e901b6981b4bbdd
blanchet <none@none>
2016-01-04 19:48:39 +0100

    more

--- mockups/Nibbles.thy
+++ mockups/Nibbles.thy
@@ -28,7 +28,7 @@ fun inc :: "nib \<Rightarrow> nib" where
 | "inc N4 = N5"
 | "inc N5 = N6"
 | "inc N6 = N7"
-| "inc N7 = N7" (* wrong *)
+| "inc N7 = N8"
 | "inc N8 = N9"
 | "inc N9 = Na"
 | "inc Na = Nb"
@@ -38,23 +38,41 @@ fun inc :: "nib \<Rightarrow> nib" where
 | "inc Ne = Nf"
 | "inc Nf = N0"
 
-(*
+fun ink :: "nib \<Rightarrow> nib" where
+  "ink N0 = N1"
+| "ink N1 = N2"
+| "ink N2 = N3"
+| "ink N3 = N4"
+| "ink N4 = N5"
+| "ink N5 = N6"
+| "ink N6 = N7"
+| "ink N7 = N7" (* wrong *)
+| "ink N8 = N9"
+| "ink N9 = Na"
+| "ink Na = Nb"
+| "ink Nb = Nc"
+| "ink Nc = Nd"
+| "ink Nd = Ne"
+| "ink Ne = Nf"
+| "ink Nf = N0"
+
 datatype byte =
   Byte nib nib
-*)
 
+(*
 type_synonym byte = "nib \<times> nib"
 
 abbreviation Byte where
   "Byte \<equiv> Pair"
+*)
 
 fun map_nibs :: "(nib \<Rightarrow> nib) \<Rightarrow> byte \<Rightarrow> byte" where
   "map_nibs f (Byte m n) = Byte (f m) (f n)"
 
-lemma "map_nibs inc b \<noteq> b"
+lemma "map_nibs ink b \<noteq> b"
 (* nunchaku *)
+(* nitpick[verbose,overlord,dont_specialize] *)
 (* nitpick[overlord, verbose, dont_box byte, card nib = 16, dont_specialize] *)
 (* nitpick[overlord, verbose, dont_box, card nib = 16] *)
-
 
 end
